d7571c4ce24b413974cc53975dd363ac18244a2f
Matt Callicott <mbcalli@clemson.edu>
2025-06-07 15:05:59 -0400

    add team stats

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from plotly.subplots import make_subplots
 from helper import *
 import json
 
-batting_tab, pitching_tab = st.tabs(["Batting", "Pitching"])
+batting_inv_tab, pitching_inv_tab, batting_team_tab, pitching_team_tab = st.tabs(["Batting - Individual", "Pitching - Individual", "Batting - Team", "Pitching - Team"])
 
-with batting_tab:
+with batting_inv_tab:
     with open("batters.json", "r") as file:
         batters = json.load(file)
 
@@ -18,7 +18,7 @@ with batting_tab:
         "Team",
         list(batters.keys()),
         index=list(batters.keys()).index("CLEM"),
-        key="batting_team"
+        key="batting_team_ind"
     )
 
     batter_name = st.selectbox(
@@ -27,7 +27,7 @@ with batting_tab:
         key="batter"
     )
 
-    if st.button("Show", key="batting_show"):
+    if st.button("Show", key="batting_show_ind"):
         connection = sqlite3.connect("baseball.db")
         raw_batting = pd.read_sql(f"SELECT * FROM raw_batting WHERE name = '{batter_name}'", connection)
         processed_batting = pd.read_sql(f"SELECT * FROM processed_batting WHERE name = '{batter_name}'", connection)
@@ -43,7 +43,7 @@ with batting_tab:
         df = pd.DataFrame(list(metrics.items()), columns=["Metric", "Description"])
         st.dataframe(df, use_container_width=True, hide_index=True)
 
-with pitching_tab:
+with pitching_inv_tab:
     with open("pitchers.json", "r") as file:
         pitchers = json.load(file)
 
@@ -53,7 +53,7 @@ with pitching_tab:
         "Team",
         list(pitchers.keys()),
         index=list(pitchers.keys()).index("CLEM"),
-        key="pitching_team"
+        key="pitching_team_ind"
     )
 
     pitcher_name = st.selectbox(
@@ -62,7 +62,7 @@ with pitching_tab:
         key="pitcher"
     )
 
-    if st.button("Show", key="pitching_show"):
+    if st.button("Show", key="pitching_show_ind"):
         connection = sqlite3.connect("baseball.db")
         raw_pitching = pd.read_sql(f"SELECT * FROM raw_pitching WHERE name = '{pitcher_name}'", connection)
         processed_pitching = pd.read_sql(f"SELECT * FROM processed_pitching WHERE name = '{pitcher_name}'", connection)
@@ -77,3 +77,84 @@ with pitching_tab:
 
         df = pd.DataFrame(list(metrics.items()), columns=["Metric", "Description"])
         st.dataframe(df, use_container_width=True, hide_index=True)
+
+
+
+
+
+
+
+
+
+
+with batting_team_tab:
+
+    with open("batters.json", "r") as file:
+        batters = json.load(file)
+
+    st.title("2025 College Baseball Batting Stats")
+
+    team_name = st.selectbox(
+        "Team",
+        list(batters.keys()),
+        index=list(batters.keys()).index("CLEM"),
+        key="batting_team_team"
+    )
+
+    with open("batting_metrics_map.json", "r") as file:
+        batting_metrics = json.load(file)
+
+    stat_name = st.selectbox(
+        "Statistic",
+        list(batting_metrics.keys()),
+        index=list(batting_metrics.keys()).index("AVG"),
+        key="batting_stat"
+    )
+
+    min_pa = st.selectbox(
+        "Minimum PA",
+        options=[0, 50, 100, 150, 200],
+        index=2
+    )
+
+    if st.button("Show", key="batting_show_team"):
+
+        fig = get_team_stat_batting_fig(team_name, batting_metrics[stat_name], stat_name, min_pa=min_pa)
+
+        st.plotly_chart(fig)
+
+with pitching_team_tab:
+
+    with open("pitchers.json", "r") as file:
+        pitchers = json.load(file)
+
+    st.title("2025 College Baseball Pitching Stats")
+
+    team_name = st.selectbox(
+        "Team",
+        list(pitchers.keys()),
+        index=list(pitchers.keys()).index("CLEM"),
+        key="pitching_team_team"
+    )
+
+    with open("pitching_metrics_map.json", "r") as file:
+        pitching_metrics = json.load(file)
+
+    stat_name = st.selectbox(
+        "Statistic",
+        list(pitching_metrics.keys()),
+        index=list(pitching_metrics.keys()).index("ERA"),
+        key="pitching_stat"
+    )
+
+    min_ip = st.selectbox(
+        "Minimum IP",
+        options=[0, 10, 20, 30, 50],
+        index=2
+    )
+
+    if st.button("Show", key="pitching_show_team"):
+
+        fig = get_team_stat_pitching_fig(team_name, pitching_metrics[stat_name], stat_name, min_ip=min_ip)
+
+        st.plotly_chart(fig)
--- helper.py
+++ helper.py
@@ -192,7 +192,6 @@ def get_pitcher_df(player_name: str, processed_pitching: pd.DataFrame, raw_pitch
     ]
 
     player_raw = raw_pitching[raw_pitching["name"] == player_name]
-    print(player_raw.columns)
     player_raw = player_raw.drop(columns=["id", "name", "team", "age", "alt_name"]).T.reset_index()
     player_raw.columns = ["stat", "value"]
     player_raw["stat"] = [
@@ -305,11 +304,220 @@ def get_player_pitching_figure(player_name: str, processed_pitching: pd.DataFram
 
     return fig
 
+def get_team_stat_batting_df(team: str, stat: str, stat_fmt: str, min_pa: int = None) -> pd.DataFrame:
 
+    connection = sqlite3.connect("baseball.db")
+    raw_team = pd.read_sql(f"SELECT * FROM raw_batting where team = '{team}'", connection)
+    processed_team = pd.read_sql(f"SELECT * FROM processed_batting where team = '{team}'", connection)
+    connection.close()
 
+    if min_pa:
+        raw_team = raw_team[raw_team["pa"] >= min_pa]
 
+    team_stat_df = pd.merge(
+        left=processed_team[["name", f"{stat.lower()}_percentile"]],
+        right=raw_team[["name", stat.lower()]],
+        on="name",
+        how="inner"
+    )
 
+    team_stat_df = team_stat_df.sort_values(by=f"{stat.lower()}_percentile", ascending=True)
 
+    team_stat_df.columns = ["name", "perc", "value"]
 
+    team_stat_df["color"] = team_stat_df["perc"].apply(perc_to_pastel_color)
+    team_stat_df["value_str"] = team_stat_df["value"].apply(value_to_string)
+
+    return team_stat_df
+
+def get_team_stat_batting_fig(team: str, stat: str, stat_fmt: str, min_pa: int = None):
+
+    team_stat_batting_df = get_team_stat_batting_df(team, stat, stat_fmt, min_pa)
+    
+    fig = make_subplots(
+        rows=1,
+        cols=2,
+        column_widths=[0.8, 0.2],
+        shared_yaxes=True,
+        horizontal_spacing=0
+    )
+
+    # Background bar (100% reference)
+    fig.add_trace(
+        go.Bar(
+            x=[100] * len(team_stat_batting_df),
+            y=team_stat_batting_df["name"],
+            orientation="h",
+            marker_color="lightgray",
+            opacity=0.3,
+            showlegend=False,
+            hoverinfo="skip"
+        ),
+        row=1, col=1
+    )
+
+    # Foreground bar (actual values)
+    fig.add_trace(
+        go.Bar(
+            x=team_stat_batting_df["perc"],
+            y=team_stat_batting_df["name"],
+            orientation="h",
+            marker_color=team_stat_batting_df["color"],
+            text=team_stat_batting_df["perc"],
+            textposition="inside",
+            textangle=0,
+            showlegend=False,
+            hoverinfo="skip"
+        ),
+        row=1, col=1
+    )
+
+    # Absolute Stats
+    fig.add_trace(
+        go.Scatter(
+            x=[0] * len(team_stat_batting_df),
+            y=team_stat_batting_df["name"],
+            mode="text",
+            text=team_stat_batting_df["value_str"],  # replace with your column
+            textposition="middle right",
+            textfont=dict(color="black", size=10),
+            showlegend=False,
+            hoverinfo="skip"
+        ),
+        row=1, col=2
+    )
+
+    fig.update_layout(
+        width=600,
+        height=600,
+        barmode="overlay",
+        plot_bgcolor="white",
+        paper_bgcolor="white",
+        # margin=dict(l=0, r=0, t=0, b=0),
+        xaxis=dict(
+            visible=False
+        ),
+        yaxis=dict(
+            showgrid=False
+        ),
+        xaxis2=dict(
+            range=[0,1],
+            visible=False
+        ),
+        yaxis2=dict(
+            showgrid=False
+        ),
+        title=fr"Team: {team} | Stat: {stat_fmt}"
+    )
+
+    return fig
+
+def get_team_stat_pitching_df(team: str, stat: str, stat_fmt: str, min_ip: int = None) -> pd.DataFrame:
+
+    connection = sqlite3.connect("baseball.db")
+    raw_team = pd.read_sql(f"SELECT * FROM raw_pitching where team = '{team}'", connection)
+    processed_team = pd.read_sql(f"SELECT * FROM processed_pitching where team = '{team}'", connection)
+    connection.close()
+
+    if min_ip:
+        raw_team = raw_team[raw_team["ip"] >= min_ip]
+
+    team_stat_df = pd.merge(
+        left=processed_team[["name", f"{stat.lower()}_percentile"]],
+        right=raw_team[["name", stat.lower()]],
+        on="name",
+        how="inner"
+    )
+
+    team_stat_df = team_stat_df.sort_values(by=f"{stat.lower()}_percentile", ascending=True)
+
+    team_stat_df.columns = ["name", "perc", "value"]
+
+    team_stat_df["color"] = team_stat_df["perc"].apply(perc_to_pastel_color)
+    team_stat_df["value_str"] = team_stat_df["value"].apply(value_to_string)
+
+    return team_stat_df
+
+def get_team_stat_pitching_fig(team: str, stat: str, stat_fmt: str, min_ip: int = None):
+
+    team_stat_pitching_df = get_team_stat_pitching_df(team, stat, stat_fmt, min_ip)
+    
+    fig = make_subplots(
+        rows=1,
+        cols=2,
+        column_widths=[0.8, 0.2],
+        shared_yaxes=True,
+        horizontal_spacing=0
+    )
+
+    # Background bar (100% reference)
+    fig.add_trace(
+        go.Bar(
+            x=[100] * len(team_stat_pitching_df),
+            y=team_stat_pitching_df["name"],
+            orientation="h",
+            marker_color="lightgray",
+            opacity=0.3,
+            showlegend=False,
+            hoverinfo="skip"
+        ),
+        row=1, col=1
+    )
+
+    # Foreground bar (actual values)
+    fig.add_trace(
+        go.Bar(
+            x=team_stat_pitching_df["perc"],
+            y=team_stat_pitching_df["name"],
+            orientation="h",
+            marker_color=team_stat_pitching_df["color"],
+            text=team_stat_pitching_df["perc"],
+            textposition="inside",
+            textangle=0,
+            showlegend=False,
+            hoverinfo="skip"
+        ),
+        row=1, col=1
+    )
+
+    # Absolute Stats
+    fig.add_trace(
+        go.Scatter(
+            x=[0] * len(team_stat_pitching_df),
+            y=team_stat_pitching_df["name"],
+            mode="text",
+            text=team_stat_pitching_df["value_str"],  # replace with your column
+            textposition="middle right",
+            textfont=dict(color="black", size=10),
+            showlegend=False,
+            hoverinfo="skip"
+        ),
+        row=1, col=2
+    )
+
+    fig.update_layout(
+        width=600,
+        height=600,
+        barmode="overlay",
+        plot_bgcolor="white",
+        paper_bgcolor="white",
+        # margin=dict(l=0, r=0, t=0, b=0),
+        xaxis=dict(
+            visible=False
+        ),
+        yaxis=dict(
+            showgrid=False
+        ),
+        xaxis2=dict(
+            range=[0,1],
+            visible=False
+        ),
+        yaxis2=dict(
+            showgrid=False
+        ),
+        title=fr"Team: {team} | Stat: {stat_fmt}"
+    )
+
+    return fig
 
 
